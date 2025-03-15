ad17ae3233398ce2e123c40ba77066b3b33e2d51
surangi soysa <surangisoysa86@gmail.com>
2025-03-16 01:54:31 +0530

    create online shop frontend

--- src/app/shop/cart.tsx
+++ src/app/shop/cart.tsx
@@ -1,9 +1,140 @@
+import { Button, Table } from 'antd'
+import { ChevronRight, Trash2 } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router'
+
+
+const columns = [
+  {
+    key: "product",
+    title: "Product Name",
+    dataIndex: "product",
+    render:(value: string)=>{
+      return <div className='flex items-center gap-5'>
+              <img src="https://m.media-amazon.com/images/I/71bNe7PVwrL._AC_UF1000,1000_QL80_.jpg" alt="" className='w-12' />
+              <h2>{value}</h2>
+      </div>
+    }
+  },
+  {
+    key: "price",
+    title: "Price",
+    dataIndex: "price"
+  },
+  {
+    key: "Qty",
+    title: "Qty",
+    dataIndex: "Qty"
+  },
+  {
+    key: "subtotal",
+    title: "Subtotal",
+    dataIndex: "subtotal"
+  },
+  {
+    key:"action",
+    dataIndex:"action",
+    render:()=>{
+      return  <Button type="dashed" htmlType='button' >  <Trash2 /></Button>
+
+    }
+  }
+]
+
+const dataSource: any = [
+  {
+    product:"Royal chain Fit",
+    price:"Rs.300.00",
+    Qty:"3",
+    subtotal:"Rs.900.00"
+
+  },
+  {
+    product:"Royal chain Fit",
+    price:"Rs.300.00",
+    Qty:"3",
+    subtotal:"Rs.900.00"
+  },
+  {
+    product:"Royal chain Fit",
+    price:"Rs.300.00",
+    Qty:"3",
+    subtotal:"Rs.900.00"
+
+  },
+  {
+    product:"Royal chain Fit",
+    price:"Rs.300.00",
+    Qty:"3",
+    subtotal:"Rs.900.00"
+
+  },
+  {
+    product:"Royal chain Fit",
+    price:"Rs.300.00",
+    Qty:"3",
+    subtotal:"Rs.900.00"
+  }
+
+];
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className='!space-y-4'>
+      <div className='flex justify-between'>
+        <div className="flex items-center">
+          <Link to="/app/dashboard" className='!text-black'>Home</Link>
+          <ChevronRight size={16} />
+          <Link to="/app/shop" className='!text-black'>Cart</Link>
+        </div>
+
+        <Button type="primary" htmlType='button' >Go Back</Button>
+      </div>
+      <div className='grid grid-cols-3 gap-5  bg-white !p-5 rounded-lg'>
+
+        <div className='col-span-2'>
+
+          <Table
+            className='!my-4'
+            columns={columns}
+            dataSource={dataSource}
+            pagination={false}
+          />
+        </div>
+
+        <div className='col-span-1'>
+
+          <div className='flex flex-col border !p-5 rounded-lg !space-y-3'>
+
+            <h2 className="text-lg font-semibold "> Cart Totals</h2>
+
+            <div className='flex gap-x-15'>
+              <h2>SKU </h2>
+              <h2>1001</h2>
+            </div>
+
+            <div className='flex gap-5'>
+              <h2>Category </h2>
+              <h2>Dog</h2>
+            </div>
+
+            <Button type="primary" htmlType='button'>Proceed to Checkout</Button>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+    </div>
+
   )
 }
 
 export default Cart
+
+
--- src/app/shop/product-view.tsx
+++ src/app/shop/product-view.tsx
@@ -1,7 +1,22 @@
+import { Button } from 'antd'
 import { ChevronRight } from 'lucide-react'
+import { useState } from 'react'
 import { Link } from 'react-router'
 
 const ProductView = () => {
+
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleQuantity = (action: string) => {
+    if (action == "increase") {
+      setQuantity(quantity + 1);
+    } else if (action == "decrease") {
+      if (quantity > 1) {
+        setQuantity(quantity - 1);
+      }
+    }
+  }
+
   return (
     <div className='!space-y-4'>
       <div className="flex items-center">
@@ -11,8 +26,58 @@ const ProductView = () => {
         <ChevronRight size={16} />
         <h2>15 kg Pedigree Pet Food, Packaging Type: Packet</h2>
       </div>
+
+
+      <div className="flex bg-white !p-5 rounded  gap-5  rounded-lg" >
+        <div className="flex flex-col items-center  w-1/2 ">
+          <img src="https://m.media-amazon.com/images/I/71bNe7PVwrL._AC_UF1000,1000_QL80_.jpg" alt="" className='w-2/5' />
+        </div>
+
+        <div className="flex flex-col gap-5 w-1/2 ">
+          <h1 className="text-3xl ">Royal food</h1>
+          <p>Pup-friendly formula with a powerful antioxidant blend for lifelong health.Pup-friendly formula with a powerful antioxidant
+            blend for lifelong health.Pup-friendly formula with a powerful antioxidant blend for lifelong health.Pup-friendly formula with a powerful antioxidant blend for lifelong health.
+          </p>
+
+          <div className='flex gap-x-15'>
+            <h2>SKU </h2>
+            <h2>1001</h2>
+          </div>
+
+          <div className='flex gap-5'>
+            <h2>Category </h2>
+            <h2>Dog</h2>
+          </div>
+
+          <div className='flex gap-x-11'>
+            <h2>Brand </h2>
+            <h2>Natural</h2>
+          </div>
+
+          <h2 className='text-xl'>Rs. 9500.00</h2>
+
+          <div className='flex gap-5'>
+
+            <div className='flex gap-3'>
+              <Button type="dashed" htmlType='button' onClick={() => handleQuantity("decrease")}>-</Button>
+              <h1 className="text-lg">{quantity}</h1>
+              <Button type="primary" htmlType='button' onClick={() => handleQuantity("increase")}>+</Button>
+            </div>
+
+            <div className='flex w-1/2'>
+              <Button type="primary" htmlType='button'>Add To Cart</Button>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
+
+
   )
 }
+
 
 export default ProductView
